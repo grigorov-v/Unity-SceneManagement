24e60a8f204e879b5dd55569d4734b79b8ddb93a
valerii.grigorov <valerii.grigorov@matryoshka.com>
2020-11-04 22:32:11 +0700

    first commit

---
+++ LoadingUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Grigorov.Unity.SceneManagement.UI {
+    public class LoadingUI : MonoBehaviour {
+        [SerializeField] Image _bar = null;
+
+        void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void UpdateBar(float progress) {
+            _bar.fillAmount = progress;
+        }
+
+        public void Show() {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide() {
+            gameObject.SetActive(false);
+        }
+    } 
+}
